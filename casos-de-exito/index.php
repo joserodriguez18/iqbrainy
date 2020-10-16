443981af19690327fec6b1068f81eead9269491f
joserodriguez18 <jmra0226@gmail.com>
2020-10-16 12:37:00 -0500

    Agregué links a las empresas en los casos de éxito y modifiqué el style.css para darle color al texto de las empresas en la misma página.

--- casos-de-exito/index.php
+++ casos-de-exito/index.php
@@ -29,7 +29,7 @@
 
     <!-- main style -->
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
-    
+
     <!-- Mission Gothic Font -->
     <link rel="stylesheet" type="text/css" href="../MissionGothic/stylesheet.css" />
 
@@ -41,118 +41,118 @@
 
     <!--=================================
         header -->
-        <?php include "../php/header.php"; ?>
+    <?php include "../php/header.php"; ?>
     <!--=================================
         header -->
     <!--=================================
         intro-title -->
-        <section class="intro-title orange-bg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="intro-content">
-                            <div class="intro-name">
-                                <h3 class="text-white">Casos de éxito</h3>
-                                <ul class="breadcrumb mt-1">
-                                    <li class="breadcrumb-item"><a href="https://iqbrainy.co/">Inicio</a></li>
-                                    <li class="breadcrumb-item active">Casos de éxito</li>
-                                </ul>
-                            </div>
-                            <div class="intro-img">
-                                <img class="img-fluid" src="../images/breadcrumb/casos-de-exito.png" alt="">
-                            </div>
+    <section class="intro-title orange-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="intro-content">
+                        <div class="intro-name">
+                            <h3 class="text-white">Casos de éxito</h3>
+                            <ul class="breadcrumb mt-1">
+                                <li class="breadcrumb-item"><a href="https://iqbrainy.co/">Inicio</a></li>
+                                <li class="breadcrumb-item active">Casos de éxito</li>
+                            </ul>
+                        </div>
+                        <div class="intro-img">
+                            <img class="img-fluid" src="../images/breadcrumb/casos-de-exito.png" alt="">
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
     <!--=================================
         intro-title -->
     <!--=================================
         case-studies  -->
-        <section class="page-section-ptb pt-6">
-            <div class="container">
-                <div class="cards-grid">
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/jlconsultores.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">JL Consultores</h4>
-                            <p class="card_ce__content">Empresa de consultoría especializada con más de 10 años de experiencia en el mercado colombiano</p>
-                        </div>
+    <section class="page-section-ptb pt-6">
+        <div class="container">
+            <div class="cards-grid">
+                <a href="https://jlconsultores.com.co" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/jlconsultores.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">JL Consultores</h4>
+                        <p class="card_ce__content">Empresa de consultoría especializada con más de 10 años de experiencia en el mercado colombiano</p>
                     </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/itlc.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">ITLC</h4>
-                            <p class="card_ce__content">Preparando personas eficaces y humanas con proyección hacia el futuro</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/jltraining.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">JL Consultores</h4>
-                            <p class="card_ce__content">Plataforma virtual de cursos sobre la consultoría empresarial</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/patriotas.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">Patriotas Boyacá</h4>
-                            <p class="card_ce__content">El Patriotas Boyacá es un club de fútbol colombiano del departamento de Boyacá</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/sermaxeco.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">Ser + Eco</h4>
-                            <p class="card_ce__content">Ayudar a transformar nuestros hábitos de consumo actual, creando consciencia para compensar positivamente nuestra huella en el planeta</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/fundacion.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">Futuros Patriotas</h4>
-                            <p class="card_ce__content">La Fundación Social Futuros Patriotas es una Entidad Sin Ánimo de Lucro creada en 2016</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/remy.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">Remy IPS</h4>
-                            <p class="card_ce__content">Institución Prestadora de Servicios de Salud, que brinda una atención de calidad, integral</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/gpslook.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">GPS Look</h4>
-                            <p class="card_ce__content">Es un emprendimiento digital que tiene como objetivo ser una pasarela para promocionar emprendimientos y empresas</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/etur.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">Etur</h4>
-                            <p class="card_ce__content">Plataforma virtual de cursos para el amor, finanzas y bienestar</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/fajasstylo.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">Fajas Stylo</h4>
-                            <p class="card_ce__content">Fajas moldeadoras para todo tipo de personas</p>
-                        </div>
-                    </div>
-                    <div class="card_ce">
-                        <img class="card_ce__icon" src="../images/clients/logos/santaLaura.png" alt="clientes-iqbrainy">
-                        <div class="card_ce__body">
-                            <h4 class="card_ce__head">Santa Laura IPS</h4>
-                            <p class="card_ce__content">Garantizar un servicio integral, seguro, humanizado con el personal idóneo</p>
-                        </div>
+                </a>
+                <div class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/itlc.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">ITLC</h4>
+                        <p class="card_ce__content">Preparando personas eficaces y humanas con proyección hacia el futuro</p>
                     </div>
                 </div>
+                <a href="https://training.jlconsultores.com.co" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/jltraining.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">JL Consultores</h4>
+                        <p class="card_ce__content">Plataforma virtual de cursos sobre la consultoría empresarial</p>
+                    </div>
+                </a>
+                <a href="https://patriotas.co/" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/patriotas.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">Patriotas Boyacá</h4>
+                        <p class="card_ce__content">El Patriotas Boyacá es un club de fútbol colombiano del departamento de Boyacá</p>
+                    </div>
+                </a>
+                <a href="http://sermaxeco.co/" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/sermaxeco.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">Ser + Eco</h4>
+                        <p class="card_ce__content">Ayudar a transformar nuestros hábitos de consumo actual, creando consciencia para compensar positivamente nuestra huella en el planeta</p>
+                    </div>
+                </a>
+                <a href="https://futurospatriotas.org/" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/fundacion.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">Futuros Patriotas</h4>
+                        <p class="card_ce__content">La Fundación Social Futuros Patriotas es una Entidad Sin Ánimo de Lucro creada en 2016</p>
+                    </div>
+                </a>
+                <a href="https://remyips.com.co/" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/remy.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">Remy IPS</h4>
+                        <p class="card_ce__content">Institución Prestadora de Servicios de Salud, que brinda una atención de calidad, integral</p>
+                    </div>
+                </a>
+                <a href="https://gpslook.co/" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/gpslook.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">GPS Look</h4>
+                        <p class="card_ce__content">Es un emprendimiento digital que tiene como objetivo ser una pasarela para promocionar emprendimientos y empresas</p>
+                    </div>
+                </a>
+                <a href="https://etur.com.co/" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/etur.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">Etur</h4>
+                        <p class="card_ce__content">Plataforma virtual de cursos para el amor, finanzas y bienestar</p>
+                    </div>
+                </a>
+                <div class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/fajasstylo.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">Fajas Stylo</h4>
+                        <p class="card_ce__content">Fajas moldeadoras para todo tipo de personas</p>
+                    </div>
+                </div>
+                <a href="https://santalauraips.com/" target="_blank" class="card_ce">
+                    <img class="card_ce__icon" src="../images/clients/logos/santaLaura.png" alt="clientes-iqbrainy">
+                    <div class="card_ce__body">
+                        <h4 class="card_ce__head">Santa Laura IPS</h4>
+                        <p class="card_ce__content">Garantizar un servicio integral, seguro, humanizado con el personal idóneo</p>
+                    </div>
+                </a>
             </div>
-        </section>
+        </div>
+    </section>
 
 
     <!--=================================
@@ -162,35 +162,34 @@
     <!--=================================
         footer -->
 
-        <?php include "../php/footer.php"; ?>
+    <?php include "../php/footer.php"; ?>
 
     <!--=================================
         footer -->
 
-        <!-- jquery  -->
-        <script type="text/javascript" src="../js/jquery.min.js"></script>
-        <script type="text/javascript" src="../js/popper.min.js"></script>
+    <!-- jquery  -->
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/popper.min.js"></script>
 
-        <!-- bootstrap -->
-        <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <!-- bootstrap -->
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
 
-        <!-- bootstrap -->
-        <script type="text/javascript" src="../js/mega-menu/mega_menu.js"></script>
+    <!-- bootstrap -->
+    <script type="text/javascript" src="../js/mega-menu/mega_menu.js"></script>
 
-        <!-- owl-carousel -->
-        <script type="text/javascript" src="../js/owl-carousel/owl.carousel.min.js"></script>
+    <!-- owl-carousel -->
+    <script type="text/javascript" src="../js/owl-carousel/owl.carousel.min.js"></script>
 
-        <!-- magnific -->
-        <script type="text/javascript" src="../js/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <!-- magnific -->
+    <script type="text/javascript" src="../js/magnific-popup/jquery.magnific-popup.min.js"></script>
 
 
-        <!-- custom -->
-        <script type="text/javascript" src="../js/custom.js"></script>
-        <script>
+    <!-- custom -->
+    <script type="text/javascript" src="../js/custom.js"></script>
+    <script>
         // Class active Li
         var li = document.getElementById('casosdeexitoLi');
         li.classList.add('active');
-
     </script>
 
 
